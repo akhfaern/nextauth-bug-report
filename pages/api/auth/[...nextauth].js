d5d943315766b146685542da02894efa7d8ac500
Murat Cem <murat.cem@bilgesecure.local>
2021-12-28 10:29:07 +0300

    Updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,7 +10,13 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.SECRET,
+
+  session: {
+    strategy: 'jwt',
+  },
   pages: {
-    signIn: "/auth/signin",
+    signIn: '/auth/signin',
+    error: '/auth/signin',
   },
 });
